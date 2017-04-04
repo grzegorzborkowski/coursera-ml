426d0a648e3f4f850317b81d8da15ddc3301189e
gborkowski <gb.grzegorzborkowski@gmail.com>
2017-04-04 23:31:02 +0200

    Add ex8 collaborative filtering cost function and gradient

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -41,13 +41,11 @@ Theta_grad = zeros(size(Theta));
 %
 
 
+ J = sum(sum(R .* (X*Theta' - Y) .^2))/2;
 
 
-
-
-
-
-
+X_grad = (R .* (X*Theta' - Y)) * Theta;
+Theta_grad = (R .* (X*Theta' - Y))' * X;
 
 
 
